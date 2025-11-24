aeefb7ca6cef39beda104157189b27c280709bac
Bilal-Delbani <bibodado313@gmail.com>
2025-11-24 18:53:55 +0200

    BILAL 24/11/2025 6:53 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import NewProject from "./Components/NewProject.jsx";
+import { useState } from "react";
 import SideBar from "./Components/SideBar.jsx";
+import NewProject from "./Components/NewProject.jsx";
+import NoProject from "./Components/NoProject.jsx";
 
 function App() {
+  const [newProject, setNewProject] = useState(false);
+
+  function handleClick() {
+    setNewProject(true);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar onClick={handleClick} />
+      {newProject ? <NewProject /> : <NoProject onClick={handleClick}/>}
     </main>
   );
 }
