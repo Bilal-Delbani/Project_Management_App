0a5d9fee921b9f6af0a3a615a48dbeb7bcdd7a3f
Bilal-Delbani <bibodado313@gmail.com>
2025-11-24 18:14:47 +0200

    BILAL 24/11/2025 6:14 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./Components/NewProject.jsx";
 import SideBar from "./Components/SideBar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8 flex flex-col gap-8">
+    <main className="h-screen my-8 flex gap-8">
       <SideBar />
+      <NewProject />
     </main>
   );
 }
