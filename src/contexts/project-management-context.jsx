94f96c7e75793b71660a49c9b33263fd680c105a
Bilal-Delbani <bibodado313@gmail.com>
2025-12-07 18:37:04 +0200

    add useReducer BILAL DELBANI 7/12/2025 6:35 PM

--- src/contexts/project-management-context.jsx
+++ src/contexts/project-management-context.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useReducer } from "react";
 
 export const ProjectManagementContext = createContext({
   newProject: [],
@@ -14,67 +14,124 @@ export const ProjectManagementContext = createContext({
   handleDeleteTask: () => {},
 });
 
-export default function ProjectManagementProvider({ children }) {
-  const [newProject, setNewProject] = useState([]);
-  // -2 : no project
-  // -1 : new project is made
-  // else: index : selected project
-  const [isNewProject, setIsNewProject] = useState(-2);
-  const [tasksCollection, setTasksCollection] = useState([]);
-
-  function handleStartProject() {
-    setIsNewProject(-1); // create new project button is clicked
-  }
-  function cancelNewProject() {
-    setIsNewProject(-2); // return to no project
-  }
-  function handleProjectSelected(index) {
-    setIsNewProject(index); // project is selected
-  }
-  function onSave(project) {
-    setNewProject((prevProjects) => [...prevProjects, project]);
-  }
-  function handleDeleteProject(projectId) {
-    setIsNewProject(-2);
-    const updatedArray = [...newProject];
+function projectManagementReducer(state, action) {
+  if (action.type === "START_NEW_PROJECT") {
+    return { ...state, isNewProject: action.isNewProject };
+  } else if (action.type === "CANCEL_NEW_PROJECT") {
+    return { ...state, isNewProject: action.isNewProject };
+  } else if (action.type === "PROJECT_SELECTED") {
+    return { ...state, isNewProject: action.isNewProject };
+  } else if (action.type === "SAVE_PROJECT") {
+    return { ...state, newProject: [...state.newProject, action.project] };
+  } else if (action.type === "DELETE_PROJECT") {
+    const updatedArray = [...state.newProject];
     const filterArray = updatedArray.filter(
-      (project) => project.id !== projectId
+      (project) => project.id !== action.projectId
     );
     const mappedArray = filterArray.map((project, index) => ({
       ...project,
       id: index,
     }));
-    setNewProject(mappedArray);
 
-    const updatedTasks = [...tasksCollection];
+    const updatedTasks = [...state.tasksCollection];
     const filteredTasks = updatedTasks.filter(
-      (task) => task.projectId !== projectId
+      (task) => task.projectId !== action.projectId
     );
     const mappedTasks = filteredTasks.map((task, index) => ({
       ...task,
       projectId: index,
     }));
-    setTasksCollection(mappedTasks);
+
+    return {
+      ...state,
+      isNewProject: action.isNewProject,
+      newProject: mappedArray,
+      tasksCollection: mappedTasks,
+    };
+  } else if (action.type === "SAVE_TASK") {
+    return {
+      ...state,
+      tasksCollection: [...state.tasksCollection, action.task],
+    };
+  } else if (action.type === "DELETE_TASK") {
+    const DeletedTask = [...state.tasksCollection];
+    const filteredDeletedTask = DeletedTask.filter(
+      (task) => task.id !== action.taskId
+    );
+    return { ...state, tasksCollection: filteredDeletedTask };
+  }
+}
+
+export default function ProjectManagementProvider({ children }) {
+  const [projectManagementState, projectManagementDispatch] = useReducer(
+    projectManagementReducer,
+    {
+      newProject: [],
+      tasksCollection: [],
+      isNewProject: -2,
+      // -2 : no project
+      // -1 : new project is made
+      // else: index : selected project
+    }
+  );
+
+  function handleStartProject() {
+    projectManagementDispatch({
+      type: "START_NEW_PROJECT",
+      isNewProject: -1, // create new project button is clicked
+    });
+  }
+  function cancelNewProject() {
+    projectManagementDispatch({
+      type: "CANCEL_NEW_PROJECT",
+      isNewProject: -2, // return to no project
+    });
+  }
+
+  function handleProjectSelected(index) {
+    projectManagementDispatch({
+      type: "PROJECT_SELECTED",
+      isNewProject: index, // project is selected
+    });
+  }
+
+  function onSave(project) {
+    projectManagementDispatch({
+      type: "SAVE_PROJECT",
+      project: project,
+    });
+  }
+
+  function handleDeleteProject(projectId) {
+    projectManagementDispatch({
+      type: "DELETE_PROJECT",
+      projectId: projectId,
+      isNewProject: -2,
+    });
   }
 
   function saveTasks(enteredTask) {
-    const task = {
-      projectId: isNewProject,
-      id: tasksCollection.length,
-      name: enteredTask,
-    };
-
-    setTasksCollection((prev) => [...prev, task]);
+    projectManagementDispatch({
+      type: "SAVE_TASK",
+      task: {
+        projectId: projectManagementState.isNewProject,
+        id: projectManagementState.tasksCollection.length,
+        name: enteredTask,
+      },
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasksCollection((tasks) => tasks.filter((task) => task.id !== taskId));
+    projectManagementDispatch({
+      type: "DELETE_TASK",
+      taskId: taskId,
+    });
   }
 
   const prjCTX = {
-    isNewProject: isNewProject,
-    newProject: newProject,
-    tasksCollection: tasksCollection,
+    isNewProject: projectManagementState.isNewProject,
+    newProject: projectManagementState.newProject,
+    tasksCollection: projectManagementState.tasksCollection,
 
     handleStartProject: handleStartProject,
     cancelNewProject: cancelNewProject,
